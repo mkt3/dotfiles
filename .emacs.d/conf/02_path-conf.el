f4ad34e8a4f877c8843f8adb9f22e3a8ffe78588
mkt3 <mkt3rb@gmail.com>
2016-12-10 16:13:48 +0900

    edit path-conf

--- .emacs.d/conf/02_path-conf.el
+++ .emacs.d/conf/02_path-conf.el
@@ -1,1 +1,5 @@
-(exec-path-from-shell-initialize)
+(let ((path-str
+           (replace-regexp-in-string
+                       "\n+$" "" (shell-command-to-string "echo $PATH"))))
+     (setenv "PATH" path-str)
+          (setq exec-path (nconc (split-string path-str ":") exec-path)))
