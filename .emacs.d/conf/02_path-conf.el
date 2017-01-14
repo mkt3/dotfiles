b724ceeae03cd7abb8a7b216254f33eb4645f510
mkt3 <mkt3rb@gmail.com>
2017-01-14 21:08:30 +0900

    move path-conf

--- .emacs.d/conf/02_path-conf.el
+++
@@ -1,5 +0,0 @@
-(let ((path-str
-           (replace-regexp-in-string
-                       "\n+$" "" (shell-command-to-string "echo $PATH"))))
-     (setenv "PATH" path-str)
-          (setq exec-path (nconc (split-string path-str ":") exec-path)))
