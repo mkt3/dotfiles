bd2a1b7aa218048f544f23812d435576294d405d
mkt3 <4037746+mkt3@users.noreply.github.com>
2017-04-21 17:38:47 +0900

    edit hiwin-color

--- .emacs.d/conf/30_hiwin-conf.el
+++ .emacs.d/conf/30_hiwin-conf.el
@@ -1,4 +1,4 @@
 (require 'hiwin)
 (hiwin-activate)                           ;; hiwin-modeを有効化
 
-(set-face-background 'hiwin-face "white80") ;; 非アクティブウィンドウの背景色を設定
+(set-face-background 'hiwin-face "gray30") ;; 非アクティブウィンドウの背景色を設定
