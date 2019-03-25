06b80106c8f8a652c2e3ee54a7f6c0005a82d4cc
mkt3 <mkt3rb@gmail.com>
2019-03-25 10:39:12 +0900

    add projectile setting

---
+++ .emacs.d/conf/24_projectile-conf.el
@@ -0,0 +1,12 @@
+(use-package helm-projectile
+  :diminish projectile-mode
+  :bind ("C-c p p" . helm-projectile-switch-project)
+  :bind ("C-c p f" . helm-projectile-find-file)
+  :bind ("C-c p e" . helm-projectile-recentf)
+  :bind ("C-c p s g" . helm-projectile-grep)
+  :bind ("C-c p h" . helm-projectile)
+  :init
+  (use-package helm-ag)
+  :config
+  (projectile-global-mode t)
+  (helm-projectile-on))
