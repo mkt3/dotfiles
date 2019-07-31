e3341b9fc15a95a3e55bfcfaf2132796348e3a83
mkt3 <mkt3rb@gmail.com>
2019-07-31 16:26:02 +0900

    Add Elpy

--- .emacs.d/conf/24_projectile-conf.el
+++ .emacs.d/conf/24_projectile-conf.el
@@ -1,12 +1,12 @@
-(use-package helm-projectile
-  :diminish projectile-mode
-  :bind ("C-c p p" . helm-projectile-switch-project)
-  :bind ("C-c p f" . helm-projectile-find-file)
-  :bind ("C-c p e" . helm-projectile-recentf)
-  :bind ("C-c p s g" . helm-projectile-grep)
-  :bind ("C-c p a" . helm-projectile)
-  :init
-  (use-package helm-ag)
-  :config
-  (projectile-global-mode t)
-  (helm-projectile-on))
+;; (use-package helm-projectile
+;;   :diminish projectile-mode
+;;   :bind ("C-c p p" . helm-projectile-switch-project)
+;;   :bind ("C-c p f" . helm-projectile-find-file)
+;;   :bind ("C-c p e" . helm-projectile-recentf)
+;;   :bind ("C-c p s g" . helm-projectile-grep)
+;;   :bind ("C-c p a" . helm-projectile)
+;;   :init
+;;   (use-package helm-ag)
+;;   :config
+;;   (projectile-global-mode t)
+;;   (helm-projectile-on))
