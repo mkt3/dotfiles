a39a9d1fa8ddb917583cc4b15459999edd0a4073
mkt3 <mkt3rb@gmail.com>
2019-09-06 18:10:55 +0900

    Add direnv-conf

--- .emacs.d/conf/25_yasnippet-conf.el
+++ .emacs.d/conf/25_yasnippet-conf.el
@@ -5,10 +5,8 @@
   (setq yas-snippet-dirs
         '("~/.dotfiles/.emacs.d/snippets"    ;; 自作スニペット
           "~/.dotfiles/.emacs.d/straight/build/yasnippet-snippets/snippets"         ;; package に含まれるスニペット
-          
           ))
-  
+
   ;; yas起動
   (yas-global-mode 1)
   )
-
