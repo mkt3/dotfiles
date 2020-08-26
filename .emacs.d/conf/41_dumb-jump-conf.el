7f5746e5fecb3d19a67dfff0ae38698038bdf5d5
mkt3 <4037746+mkt3@users.noreply.github.com>
2020-08-27 05:56:21 +0900

    Change from makefile to ansible

--- .emacs.d/conf/41_dumb-jump-conf.el
+++
@@ -1,7 +0,0 @@
-  (use-package dumb-jump
-    :bind (("M-g j" . dumb-jump-go)
-           ("M-g k" . dumb-jump-back)
-           ("M-g l" . dumb-jump-quick-look))
-    :config (setq dumb-jump-selector 'helm)
-    )
-
