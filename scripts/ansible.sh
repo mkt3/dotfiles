9bab054100f69b05ad5380590373ea607798595f
mkt3 <4037746+mkt3@users.noreply.github.com>
2020-08-28 07:02:53 +0900

    Add setup.sh

---
+++ scripts/ansible.sh
@@ -0,0 +1,26 @@
+  
+#!/bin/bash
+
+set -e
+
+os=$(uname -s | tr '[A-Z]' '[a-z]')
+
+case $os in
+    darwin)
+        PLAYBOOK=ansible-playbook/mac.yml
+    ;;
+    linux)
+        dist=$(cat /etc/issue | tr '[A-Z]' '[a-z]')
+        case $dist in
+            arch*)
+                PLAYBOOK=ansible-playbook/arch.yml
+            ;;
+            ubuntu*)
+                PLAYBOOK=ansible-playbook/ubuntu.yml
+            ;;
+         esac
+    ;;
+esac
+
+echo "run playbook: $PLAYBOOK"
+ansible-playbook --verbose -i ansible-playbook/local $PLAYBOOK
---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -eu
+
+./scripts/ansible.sh
