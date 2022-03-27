05ea3b6ec322621eae81954482db04c409290636
mkt3 <4037746+mkt3@users.noreply.github.com>
2022-03-27 22:20:57 +0900

    Fix bug

--- scripts/pacman/setup.sh
+++ scripts/pacman/setup.sh
@@ -5,10 +5,16 @@ set -eu
 setup_pacman() {
     title "Setting up pacman"
 
+    installed_package=`sudo pacman -Qe`
     package_list=(python \
+                  python-pip \
                   python-pipx
                       )
 
     info "Installing packages"
-    sudo pacman -S ${package_list[@]} --noconfirm
+    for package in ${package_list[@]}; do
+        if [ ! "$(echo ${installed_package} | grep "^${package} " )" ] ;then
+            sudo pacman -S $package --noconfirm
+        fi
+    done
 }
