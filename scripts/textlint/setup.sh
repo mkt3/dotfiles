fdbec091bb5f0b3f235ebfedd27ed01375f6eea3
mkt3 <4037746+mkt3@users.noreply.github.com>
2022-05-27 11:39:37 +0900

    Add option

--- scripts/textlint/setup.sh
+++ scripts/textlint/setup.sh
@@ -14,15 +14,15 @@ setup_textlint() {
     ln -sfn "${textlint_file_dir}/textlint.sh" "${HOME}/.local/bin/textlint.sh"
 
     info "Installing textlint's packages"
-    installed_package=`npm ls -g`
+    installed_package=`npm ls --location=global`
     package_list=(textlint textlint-plugin-org traverse textlint-rule-preset-ja-technical-writing textlint-rule-preset-ja-spacing textlint-rule-alex textlint-rule-common-misspellings textlint-rule-ginger textlint-rule-write-good textlint-rule-en-max-word-count)
     for package in ${package_list[@]}; do
         if [ "$(echo ${installed_package} | grep "${package}" )" ] ;then
             info "$package updating..."
-            npm update -g $package
+            npm update --location=global $package
         else
             info "$package installting..."
-            npm install -g $package
+            npm install --location=global $package
         fi
     done
 }
