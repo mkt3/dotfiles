0f5af40e8c4b6fc21a130220e973467783bbc0c4
Makoto Morinaga <4037746+mkt3@users.noreply.github.com>
2022-06-03 21:34:48 +0900

    Add navi

---
+++ scripts/navi/setup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -eu
+
+setup_navi() {
+    title "Setting up navi"
+    local navi_dir="$CONFIGS_DIR/navi"
+
+    "${CARGO_HOME}/bin/rustup" run stable cargo install navi
+
+    info "Creating symlink for navi"
+    ln -sfn ${navi_dir} "${XDG_CONFIG_HOME}/navi"
+
+    local mac_config_dir="${HOME}/Library/Application Support"
+    if [ -e "${mac_config_dir}" ]; then
+        eval "ln -sfn ${navi_dir} \"${mac_config_dir}/navi\""
+    fi
+
+}
