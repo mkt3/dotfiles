c25e3f64cce90ecc625954c6e37041bb37ed7b5e
Makoto Morinaga <makoto@mkt3.me>
2022-08-12 13:34:38 +0900

    Add pager

--- scripts/lazygit/setup.sh
+++ scripts/lazygit/setup.sh
@@ -7,4 +7,9 @@ setup_lazygit() {
 
     info "Installing lazygit"
     go install github.com/jesseduffield/lazygit@latest
+
+    mkdir -p "${XDG_CONFIG_HOME}/lazygit"
+    info "Creating symlink for lazygit"
+    ln -sfn "${CONFIGS_DIR}/lazygit/config.yml" "${XDG_CONFIG_HOME}/lazygit/"
+
 }
