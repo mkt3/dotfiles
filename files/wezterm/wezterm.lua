cf9cecde777ad7612890eaec9316acb2e3a6af0e
Makoto Morinaga <makoto@mkt3.me>
2022-08-27 02:41:13 +0900

    Add wezterm

---
+++ files/wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require 'wezterm';
+
+return {
+  font = wezterm.font("Cica"),
+  use_ime = true,
+  font_size = 20.0,
+  color_scheme = "Japanesque",
+  hide_tab_bar_if_only_one_tab = true,
+  adjust_window_size_when_changing_font_size = false,
+}
---
+++ scripts/wezterm/setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -eu
+
+setup_wezterm() {
+    title "Setting up wezterm"
+    local wezterm_file_dir="${CONFIGS_DIR}/wezterm"
+
+    info "Installing wezterm"
+    install_pacman_package wezterm
+
+    info "Creating symlink for wezterm"
+    ln -sfn $wezterm_file_dir $XDG_CONFIG_HOME
+}
