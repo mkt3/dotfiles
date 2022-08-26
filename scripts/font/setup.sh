932eed192722ac1591425f4fdd9f45c72071e760
Makoto Morinaga <makoto@mkt3.me>
2022-08-27 02:42:08 +0900

    Update download link

--- scripts/font/setup.sh
+++ scripts/font/setup.sh
@@ -6,17 +6,17 @@ setup_font() {
     title "Setting up font"
 
     info "Add cica conf"
-    local font_dir="/usr/share/fonts/Cica"
+    local font_dir="${XDG_DATA_HOME}/fonts/Cica"
     local font_name="Cica"
 
-    if bash -lc "fc-match | grep ${font_name} > /dev/null"; then
+    if bash -lc "fc-match $font_name | grep ${font_name} > /dev/null"; then
         info "Cica font already exists... Skipping."
         return 0
     fi
 
     sudo mkdir -p $font_dir
     local cica_download_path="/tmp/Cica.zip"
-    sudo curl -fL -o $cica_download_path https://github.com/miiton/Cica/releases/download/v5.0.2/Cica_v5.0.2_with_emoji.zip
+    sudo curl -fL -o $cica_download_path https://github.com/miiton/Cica/releases/download/v5.0.3/Cica_v5.0.3.zip
 
     sudo unzip -d $font_dir $cica_download_path
 
