58a4b32e5a691148bd9b1043e28172e501ebd1f7
Makoto Morinaga <makoto@mkt3.me>
2022-08-27 02:41:57 +0900

    Fix typo

--- scripts/skk/setup.sh
+++ scripts/skk/setup.sh
@@ -6,7 +6,7 @@ setup_skk() {
     title "Setting up skk"
 
     info "Creating symlink for zsh"
-    local skk_rule_path="${XDG_CONFIG/HOME}/libskk/rules"
+    local skk_rule_path="${XDG_CONFIG_HOME}/libskk/rules"
     mkdir -p $skk_rule_path
 
     ln -sfn "${CONFIGS_DIR}/skk/StickyShift" "${skk_rule_path}/StickyShift"
