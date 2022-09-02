f5a462677e3caeeac3b3a7462893a8872541a9c5
Makoto <makoto@mkt3.me>
2022-09-02 09:43:55 +0900

    Add gruvbox theme

--- files/wezterm/wezterm.lua
+++ files/wezterm/wezterm.lua
@@ -4,7 +4,7 @@ return {
   font = wezterm.font("Cica"),
   use_ime = true,
   font_size = 20.0,
-  color_scheme = "Japanesque",
+  color_scheme = "Gruvbox dark, medium (base16)",
   hide_tab_bar_if_only_one_tab = true,
   adjust_window_size_when_changing_font_size = false,
 }
