3d4d535eb5faf02708b3f9e9b1e403bf999b4133
Makoto Morinaga <makoto@mkt3.me>
2022-09-07 11:44:14 +0900

    Change font size

--- files/wezterm/wezterm.lua
+++ files/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require 'wezterm';
 return {
   font = wezterm.font("Cica"),
   use_ime = true,
-  font_size = 20.0,
+  font_size = 16.0,
   color_scheme = "Gruvbox dark, medium (base16)",
   hide_tab_bar_if_only_one_tab = true,
   adjust_window_size_when_changing_font_size = false,
