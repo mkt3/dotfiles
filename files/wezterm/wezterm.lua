cfbeac3f743f06679c0ce2d8da6b29b9adf2076b
Makoto Morinaga <makoto@mkt3.me>
2022-09-28 17:32:25 +0900

    For emacs in terminal

--- files/wezterm/wezterm.lua
+++ files/wezterm/wezterm.lua
@@ -7,4 +7,14 @@ return {
   color_scheme = "nord",
   hide_tab_bar_if_only_one_tab = true,
   adjust_window_size_when_changing_font_size = false,
+  keys = {
+     {key="x",mods="SUPER",action=wezterm.action.SendKey { key="x", mods="ALT"}},
+     {key="c",mods="SUPER",action=wezterm.action.SendKey { key="c", mods="ALT"}},
+     {key="w",mods="SUPER",action=wezterm.action.SendKey { key="w", mods="ALT"}},
+     {key=",",mods="SUPER",action=wezterm.action.SendKey { key=",", mods="ALT"}},
+     {key=".",mods="SUPER",action=wezterm.action.SendKey { key=".", mods="ALT"}},
+     {key=";",mods="SUPER",action=wezterm.action.SendKey { key=";", mods="ALT"}},
+     {key="/",mods="SUPER",action=wezterm.action.SendKey { key="/", mods="ALT"}},
+     {key="?",mods="SUPER|SHIFT",action=wezterm.action.SendKey { key="?", mods="ALT"}},
+  },
 }
