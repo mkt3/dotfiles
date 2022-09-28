5c3ada72420bf5ffc42667018d10fe44e09a28ce
Makoto Morinaga <makoto@mkt3.me>
2022-09-28 11:51:10 +0900

    Enable nord theme

--- files/wezterm/wezterm.lua
+++ files/wezterm/wezterm.lua
@@ -4,7 +4,8 @@ return {
   font = wezterm.font("Cica"),
   use_ime = true,
   font_size = 16.0,
-  color_scheme = "Gruvbox dark, medium (base16)",
+  color_scheme = "nord",
   hide_tab_bar_if_only_one_tab = true,
   adjust_window_size_when_changing_font_size = false,
+  window_background_opacity = 0.95,
 }
