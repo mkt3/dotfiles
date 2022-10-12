c8a227c860b8da8c9f7fb5ee127ca4c304425a10
Makoto Morinaga <makoto@mkt3.me>
2022-10-12 11:22:05 +0900

    Change conf

--- files/wezterm/wezterm.lua
+++ files/wezterm/wezterm.lua
@@ -5,7 +5,6 @@ return {
   use_ime = true,
   font_size = 16.0,
   color_scheme = "nord",
-  hide_tab_bar_if_only_one_tab = true,
   adjust_window_size_when_changing_font_size = false,
   window_padding = {
     left = 0,
