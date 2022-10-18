5eb533a84ca45147f323b8b855135f197c93380f
Makoto Morinaga <makoto@mkt3.me>
2022-10-19 00:18:30 +0900

    Add env

--- scripts/xwindow/setup.sh
+++ scripts/xwindow/setup.sh
@@ -7,6 +7,7 @@ setup_xwindow() {
     local xwindow_file_dir="${CONFIGS_DIR}/xwindow"
 
     info "Creating symlink for .xprofile"
+    # for lightdm
     ln -sfn "${xwindow_file_dir}/xprofile" "${HOME}/.xprofile"
 
     info "Creating symlink for .Xdefaults"
