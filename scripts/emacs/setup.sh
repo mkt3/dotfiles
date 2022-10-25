53fa354d33e80e29ce60930a3c72540dd62fd1ab
Makoto Morinaga <makoto@mkt3.me>
2022-10-25 12:21:09 +0900

    Fix typo

--- scripts/emacs/setup.sh
+++ scripts/emacs/setup.sh
@@ -9,9 +9,10 @@ setup_emacs() {
 
     mkdir -p "${XDG_CONFIG_HOME}/emacs/ddskk.d"
     info "Creating symlink for emacs"
-    ln -sfn $emacs_file_dir/README.org "${XDG_CONFIG_HOME}/emacs/"
-    ln -sfn $emacs_file_dir/init.el "${XDG_CONFIG_HOME}/emacs/"
-    ln -sfn $emacs_file_dir/snippets "${XDG_CONFIG_HOME}/emacs/"
+    ln -sfn "${emacs_file_dir}/README.org" "${XDG_CONFIG_HOME}/emacs/"
+    ln -sfn "${emacs_file_dir}/init.el" "${XDG_CONFIG_HOME}/emacs/"
+    ln -sfn "${emacs_file_dir}/snippets" "${XDG_CONFIG_HOME}/emacs/"
+
 
     if [ ! -e $skk_record_file ];then
         touch $skk_record_file
