ef89c9eb9a702d9d9517e22993fb532efcd65966
Makoto Morinaga <makoto@mkt3.me>
2022-10-31 21:12:35 +0900

    Add kanshi

--- scripts/sway/setup.sh
+++ scripts/sway/setup.sh
@@ -8,7 +8,8 @@ setup_sway() {
     local swaylock_file_dir="${CONFIGS_DIR}/swaylock"
     local waybar_file_dir="${CONFIGS_DIR}/waybar"
     local rofi_file_dir="${CONFIGS_DIR}/rofi"
-
+    local kanshi_file_dir="${CONFIGS_DIR}/kanshi"
+    
     info "Creating symlink for sway"
     ln -sfn "${sway_file_dir}" "${XDG_CONFIG_HOME}"
 
@@ -21,9 +22,12 @@ setup_sway() {
     info "Creating symlink for rofi"
     ln -sfn "${rofi_file_dir}" "${XDG_CONFIG_HOME}"
 
-    info "Adding systemd"
+    info "Adding sway-session.target"
     mkdir -p "${XDG_CONFIG_HOME}/systemd/user"
     ln -sfn "${sway_file_dir}/sway-session.target" "${XDG_CONFIG_HOME}/systemd/user/"
 
+    info "Adding kanshi"
+    ln -sfn "${kanshi_file_dir}" "${XDG_CONFIG_HOME}"
+    ln -sfn "${kanshi_file_dir}/kanshi.service" "${XDG_CONFIG_HOME}/systemd/user/"
 
 }
