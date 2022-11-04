0774378fd5853df0cbd74d644450e07015929c16
Makoto Morinaga <makoto@mkt3.me>
2022-11-04 14:45:48 +0900

    Add system menu

---
+++ files/rofi/scripts/system_menu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+declare -A SYSTEM_DICT=(
+    ["Reboot"]="systemctl reboot"
+    ["Suspend"]="systemctl suspend"
+    ["Poweroff"]="systemctl poweroff"
+)
+
+IFS=$'\n'
+if [[ $# -ne 0 ]]; then
+    eval "${SYSTEM_DICT[$1]}" &> /dev/null
+    exit 0
+else
+    echo "${!SYSTEM_DICT[*]}"
+fi
+
