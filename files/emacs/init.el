813ed23c248f3915e434f130e47992a77d069163
Makoto Morinaga <makoto@mkt3.me>
2022-12-02 00:25:02 +0900

    Silence compiler warnings

--- files/emacs/init.el
+++ files/emacs/init.el
@@ -1,5 +1,8 @@
 (setq vc-follow-symlinks t)
 
+;; Silence compiler warnings
+(setq native-comp-async-report-warnings-errors 'silent)
+
 (require 'org)
 (org-babel-load-file
  (expand-file-name "README.org" user-emacs-directory))
