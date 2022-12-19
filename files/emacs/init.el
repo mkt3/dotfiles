0a9f98119c372da33e3730543d29343d0d273238
Makoto Morinaga <makoto@mkt3.me>
2022-12-19 10:26:43 +0900

    Add compile

--- files/emacs/init.el
+++ files/emacs/init.el
@@ -1,8 +1,15 @@
+(defconst my-saved-file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
 (setq vc-follow-symlinks t)
 
 ;; Silence compiler warnings
 (setq native-comp-async-report-warnings-errors 'silent)
 
+(native-compile-async "~/.config/emacs/ddskk.d/init.el")
+
 (require 'org)
 (org-babel-load-file
  (expand-file-name "README.org" user-emacs-directory))
+
+(setq file-name-handler-alist my-saved-file-name-handler-alist)
