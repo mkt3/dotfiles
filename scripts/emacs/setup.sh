2d2c302678fd658b0d311abf562b4bdbe82827e5
Makoto Morinaga <makoto@mkt3.me>
2023-01-02 01:06:32 +0900

    Fix typo

--- scripts/emacs/setup.sh
+++ scripts/emacs/setup.sh
@@ -14,9 +14,11 @@ setup_emacs() {
     ln -sfn "${emacs_file_dir}/snippets" "${XDG_CONFIG_HOME}/emacs/"
     ln -sfn "${emacs_file_dir}/ddskk.d/init.el" "${XDG_CONFIG_HOME}/emacs/ddskk.d/"
 
-    if [ ! -e $skk_record_file ];then
-       touch $skk_record_file
+    if [ ! -e $skk_record_file ]; then
+        touch $skk_record_file
     fi
 
     mkdir -p "${XDG_CONFIG_HOME}/msmtp"
     ln -sfn "${HOME}/Nextcloud/personal_config/emacs_mail/msmtprc" "${XDG_CONFIG_HOME}/msmtp/config"
+
+}
