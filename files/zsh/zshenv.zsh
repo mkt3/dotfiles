4c455068df3ec918036f82bb42a943f161596fc5
Makoto Morinaga <makoto@mkt3.me>
2023-03-28 17:25:42 +0900

    Fix typo

--- files/zsh/zshenv.zsh
+++ files/zsh/zshenv.zsh
@@ -1,4 +1,4 @@
-if [[ $SETUP == "TRUE" ]];then
+if [[ "$SETUP" == "TRUE" ]];then
     echo "loading from setup script"
 else
     setopt no_global_rcs
@@ -6,15 +6,15 @@ fi
 
 # Language
 export LANGUAGE="en_US.UTF-8"
-export LANG="${LANGUAGE}"
-export LC_ALL="${LANGUAGE}"
-export LC_CTYPE="${LANGUAGE}"
+export LANG="$LANGUAGE"
+export LC_ALL="$LANGUAGE"
+export LC_CTYPE="$LANGUAGE"
 
 # Editor
 export EDITOR=vim
-export CVSEDITOR="${EDITOR}"
-export SVN_EDITOR="${EDITOR}"
-export GIT_EDITOR="${EDITOR}"
+export CVSEDITOR="$EDITOR"
+export SVN_EDITOR="$EDITOR"
+export GIT_EDITOR="$EDITOR"
 
 # Pager
 export PAGER=less
@@ -32,13 +32,13 @@ fi
 export PLATFORM
 
 # PATH
-export XDG_CONFIG_HOME="$HOME/.config"
-export XDG_CACHE_HOME="$HOME/.cache"
-export XDG_DATA_HOME="$HOME/.local/share"
-export XDG_STATE_HOME="$HOME/.local/state"
+export XDG_CONFIG_HOME="${HOME}/.config"
+export XDG_CACHE_HOME="${HOME}/.cache"
+export XDG_DATA_HOME="${HOME}/.local/share"
+export XDG_STATE_HOME="${HOME}/.local/state"
 
 # zsh
-ZDOTDIR="$XDG_CONFIG_HOME/zsh"
+ZDOTDIR="${XDG_CONFIG_HOME}/zsh"
 ZSH_COMPLETION_DIR="${XDG_DATA_HOME}/zsh/completion"
 export LISTMAX=1000
 
@@ -50,16 +50,16 @@ if [[ "$PLATFORM" == 'osx' ]];then
     MAC_DEFAULT_PATH="/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin"
     export SHELL_SESSIONS_DISABLE=1
 
-    export HOMEBREW_BUNDLE_FILE="$XDG_CONFIG_HOME/homebrew/Brewfile"
+    export HOMEBREW_BUNDLE_FILE="${XDG_CONFIG_HOME}/homebrew/Brewfile"
 
     if [[ "$ARCH" == 'arm64' ]]; then
-        export PATH="/opt/homebrew/bin:$MAC_DEFAULT_PATH"
-        export PATH="/opt/homebrew/opt/texinfo/bin:$PATH"
+        export PATH="/opt/homebrew/bin:${MAC_DEFAULT_PATH}"
+        export PATH="/opt/homebrew/opt/texinfo/bin:${PATH}"
         export LIBRARY_PATH="/opt/homebrew/opt/libgccjit/lib/gcc/11"
         export CPATH="/opt/homebrew/Cellar/libgccjit/11.2.0_1/include"
     elif [[ "$ARCH" == 'x86_64' ]]; then
-        export PATH="/usr/local/bin:$MAC_DEFAULT_PATH"
-        export PATH="/usr/local/opt/texinfo/bin:$PATH"
+        export PATH="/usr/local/bin:${MAC_DEFAULT_PATH}"
+        export PATH="/usr/local/opt/texinfo/bin:${PATH}"
         export LIBRARY_PATH="/usr/local/opt/libgccjit/lib/gcc/11"
 
     fi
@@ -71,16 +71,16 @@ elif [[ "$PLATFORM" == 'linux' ]];then
     export DOCKER_HOST=unix:///run/user/`id $(whoami) | awk -F'[=()]' '{print $2}'`/docker.sock
 fi
 
-export PYTHONUSERBASE="$HOME/.local"
+export PYTHONUSERBASE="${HOME}/.local"
 
 # Pyenv
-export PYENV_ROOT="$XDG_DATA_HOME/pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
+export PYENV_ROOT="${XDG_DATA_HOME}/pyenv"
+export PATH="${PYENV_ROOT}/bin:${PATH}"
 
 # Poety path
-export PATH="$HOME/.poetry/bin:$PATH"
+export PATH="${HOME}/.poetry/bin:${PATH}"
 
-export PATH="$HOME/.local/bin:$PATH"
+export PATH="${HOME}/.local/bin:${PATH}"
 
 export PYTHONDONTWRITEBYTECODE=1
 
@@ -92,36 +92,36 @@ export JUPYTER_CONFIG_DIR="${XDG_CONFIG_HOME}/jupyter"
 export JUPYTER_DATA_DIR="${XDG_DATA_HOME}/jupyter"
 
 # RUSTUP & Cargo path
-export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
-export CARGO_HOME="$XDG_DATA_HOME/cargo"
-[ -d "$CARGO_HOME" ] && . "$CARGO_HOME/env"
+export RUSTUP_HOME="${XDG_DATA_HOME}/rustup"
+export CARGO_HOME="${XDG_DATA_HOME}/cargo"
+[ -d "$CARGO_HOME" ] && . "${CARGO_HOME}/env"
 
 # nvm path
-export NVM_DIR="$XDG_CONFIG_HOME/nvm"
+export NVM_DIR="${XDG_CONFIG_HOME}/nvm"
 export PATH="${XDG_CONFIG_HOME}/nvm/versions/node/default/bin:${PATH}"
 
 # npm path
-export NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npm/npmrc
+export NPM_CONFIG_USERCONFIG="${XDG_CONFIG_HOME}/npm/npmrc"
 
 # cuda path
 if [[ "$PLATFORM" == 'linux' ]];then
-    export PATH="$PATH:/opt/cuda/bin:/usr/local/cuda-11.3/bin"
+    export PATH="${PATH}:/opt/cuda/bin:/usr/local/cuda-11.3/bin"
     export CUDA_CACHE_PATH="${XDG_CACHE_HOME}/nv"
 fi
 
 # docker
-export DOCKER_CONFIG="$XDG_CONFIG_HOME/docker"
+export DOCKER_CONFIG="${XDG_CONFIG_HOME}/docker"
 
 # vim
 export VIMINIT='let $MYVIMRC="$XDG_CONFIG_HOME/vim/vimrc" | source $MYVIMRC'
 
 # enhancd
-export ENHANCD_DIR="$XDG_DATA_HOME/enhancd"
+export ENHANCD_DIR="${XDG_DATA_HOME}/enhancd"
 
 # terminfo
-export TERMINFO="$XDG_DATA_HOME/terminfo"
-export TERMINFO_DIRS="$XDG_DATA_HOME/terminfo:/usr/share/terminfo"
-export COLORTERM=truecolor
+export TERMINFO="${XDG_DATA_HOME}/terminfo"
+export TERMINFO_DIRS="${XDG_DATA_HOME}/terminfo:/usr/share/terminfo"
+export COLORTERM="truecolor"
 
 # go
 export GOPATH="${XDG_DATA_HOME}/go"
@@ -157,3 +157,9 @@ export XAPIAN_CJK_NGRAM=1
 
 # sqlite
 export SQLITE_HISTORY="${XDG_DATA_HOME}/sqlite_history"
+
+# dvdcss
+export DVDCSS_CACHE="${XDG_DATA_HOME}/dvdcss"
+
+# wget
+export WGETRC="${XDG_CONFIG_HOME}/wgetr"
