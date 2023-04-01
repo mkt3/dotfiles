d35aa5bfa8b4e6c05613811d5df9a54320081685
Makoto Morinaga <makoto@mkt3.me>
2023-04-01 21:14:07 +0900

    Change ssh.sh

--- files/wezterm/ssh.sh
+++ files/wezterm/ssh.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
-SSH_CONFIG_FILE_LIST=`bash -c "ls ~/.ssh/*/config" 2> /dev/null`
+
+ssh_config_file=`bash -c "ls ~/.ssh/config" 2> /dev/null`
 host_list=""
-for ssh_config in ${SSH_CONFIG_FILE_LIST}
+for i in `grep "^Host " $ssh_config_file | grep -v "*" | sed s/"^Host "// | sed s/","/\ /g`
 do
-    for i in `grep "^Host " $ssh_config | grep -v "*" | sed s/"^Host "// | sed s/","/\ /g`
-    do
-        host_list="${host_list}${i}\n"
-    done
+    host_list="${host_list}${i}\n"
 done
 ssh_host="`echo -e $host_list | ~/.local/share/fzf/bin/fzf --reverse --border --ansi --prompt='Server:' | cut -d: -f1`"
 if [[ "$ssh_host" = "" ]]; then
