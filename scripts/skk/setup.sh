3b42767e5cbdafb397cfa9e579ec314cacb49898
Makoto Morinaga <makoto@mkt3.dev>
2023-08-09 00:23:23 +0900

    Enable skk repo

--- scripts/skk/setup.sh
+++ scripts/skk/setup.sh
@@ -14,14 +14,14 @@ setup_skk() {
     info "Building yaskkserv2"
     local yaskkserv2_repo_path="${HOME}/.local/src/yaskkserv2"
     local yaskkserv2_dict_path="${XDG_DATA_HOME}/yaskkserv2"
-    local skk_dict_dir_path="${HOME}/Nextcloud/personal_config/skk/downloaded_skk_dict"
-    local skk_dict_url_list=("https://skk-dev.github.io/dict/SKK-JISYO.L.gz"
-                             "https://skk-dev.github.io/dict/SKK-JISYO.jinmei.gz"
-                             "https://skk-dev.github.io/dict/SKK-JISYO.propernoun.gz")
+    local skk_dict_repo_path="${HOME}/Nextcloud/personal_config/skk/dict"
 
     local skk_dict_path_list=("${HOME}/Nextcloud/personal_config/skk/aquaskk/skk-jisyo.utf8"
                               "${HOME}/Nextcloud/personal_config/skk/fcitx5skk/user.dict"
-                              "${HOME}/Nextcloud/personal_config/skk/ddskk/skk-jisyo.utf8")
+                              "${HOME}/Nextcloud/personal_config/skk/ddskk/skk-jisyo.utf8"
+                              "${skk_dict_repo_path}/SKK-JISYO.L"
+                              "${skk_dict_repo_path}/SKK-JISYO.jinmei"
+                              "${skk_dict_repo_path}/SKK-JISYO.propernoun")
 
     if [ -d "$yaskkserv2_repo_path" ]; then
         cd "$yaskkserv2_repo_path"
@@ -30,21 +30,19 @@ setup_skk() {
         git clone https://github.com/wachikun/yaskkserv2.git "$yaskkserv2_repo_path"
     fi
 
-    local dict_name=""
-    for url in "${skk_dict_url_list[@]}"; do
-        dict_name=$(basename "$url" .gz)
-        local skk_dict_path="${skk_dict_dir_path}/${dict_name}"
-        skk_dict_path_list+=("$skk_dict_path")
-        if [ ! -e "$skk_dict_path" ]; then
-            wget -O - "$url" | gzip -d > "$skk_dict_path"
-        fi
-    done
-
     cd "$yaskkserv2_repo_path"
     cargo build --release
 
     ln -sfn "${yaskkserv2_repo_path}/target/release/yaskkserv2" "${HOME}/.local/bin"
     ln -sfn "${yaskkserv2_repo_path}/target/release/yaskkserv2_make_dictionary" "${HOME}/.local/bin"
+
+    info "Updating skk dictionary"
+    if [ -d "$skk_dict_repo_path" ]; then
+        cd "$skk_dict_repo_path"
+        git pull
+    else
+        git clone https://github.com/skk-dev/dict.git "$skk_dict_repo_path"
+    fi
 
     info "Building yaskkserv2 dictionary"
     mkdir -p "$yaskkserv2_dict_path"
