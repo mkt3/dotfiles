908aaaeccae69c9410cde47a2aa729be4b608a64
Makoto Morinaga <makoto@mkt3.dev>
2023-12-07 12:13:04 +0900

    Fix typo

--- files/wezterm/wezterm.lua
+++ files/wezterm/wezterm.lua
@@ -30,7 +30,7 @@ end
 
 return {
   font = wezterm.font_with_fallback {
-    { family = 'PlemolJP', assume_emoji_presentation = false},
+    { family = 'PlemolJP Console NF', assume_emoji_presentation = false},
     { family = 'Symbols Nerd Font Mono', assume_emoji_presentation = false},
     { family = 'Cica', assume_emoji_presentation = true},
     { family = 'Noto Emoji', assume_emoji_presentation = true},
