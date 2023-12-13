e3ac5a7e205bc6981fe16c0abcb5bf8edf133a69
Makoto Morinaga <makoto@mkt3.dev>
2023-12-13 22:39:58 +0900

    Update misc

--- files/emacs/build_emacs.sh
+++ files/emacs/build_emacs.sh
@@ -41,7 +41,7 @@ case "$OS" in
                 sudo pacman -S --needed $DEPENDENCIES
                 ;;
             Ubuntu)
-                DEPENDENCIES="autoconf make texinfo gnutls-bin libgccjit-12-dev libacl1 libc6 libdbus-1-3 libgmp10 libgnutls28-dev libgnutls30 libgpm2 libjansson-dev liblcms2-2 libsystemd0 libtinfo6 libxml2 libxml2-dev zlib1g libtree-sitter-dev libtool-bin enchant-2 nuspell libnuspell-dev hunspell-en-us"
+                DEPENDENCIES="autoconf make texinfo gnutls-bin libgccjit-12-dev libacl1 libc6 libdbus-1-3 libgmp10 libgnutls28-dev libgnutls30 libgpm2 libjansson-dev liblcms2-2 libsystemd0 libtinfo6 libxml2 libxml2-dev zlib1g libtree-sitter-dev libtool-bin pkgconf libenchant-2-dev enchant-2 nuspell libnuspell-dev hunspell-en-us"
                 if [[ "$UI" == "gui" ]]; then
                     DEPENDENCIES="${DEPENDENCIES} libcairo2 libfontconfig1 libfreetype6 libgdk-pixbuf-2.0-0 libgif7 libglib2.0-0 libgtk-3-0 libharfbuzz0b libjpeg8 libm17n-0 libotf1 libpango-1.0-0 libpng16-16 libsvg2-2 libsm6 libtiff5 imagemagick"
                 fi
