3792987b9e4d2ea0937b382eae396320d6fe28c2
Makoto Morinaga <makoto@mkt3.dev>
2024-02-07 11:37:58 +0900

    Update misc

--- files/wezterm/wezterm.lua
+++ files/wezterm/wezterm.lua
@@ -24,7 +24,7 @@ local keys = {
   {key="j",mods="CTRL",action=wezterm.action.SendString "\x18@j"}, -- for emacs ddskk interminal
   {key="v",mods="OPT",action=wezterm.action.PasteFrom 'Clipboard'},
   {key="w",mods="OPT",action=wezterm.action.CloseCurrentTab{confirm=false}},
-  {key="o",mods="CMD",action=wezterm.action.SpawnCommandInNewTab{args={".config/wezterm/ssh.sh"},cwd = '~'}},
+  {key="o",mods="CMD",action=wezterm.action.SpawnCommandInNewTab{args={"./.config/wezterm/ssh.sh"},cwd = '~'}},
 }
 
 font_size = 18.0
