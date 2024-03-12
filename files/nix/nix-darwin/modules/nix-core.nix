5fb8e613ba5f17edbb561a461b984456fe12eb03
Makoto Morinaga <makoto@mkt3.dev>
2024-03-12 11:06:26 +0900

    Enable hunspell in global

--- files/nix/nix-darwin/modules/nix-core.nix
+++ files/nix/nix-darwin/modules/nix-core.nix
@@ -1,5 +1,10 @@
-{  ... }:
+{ pkgs, ... }:
 {
   services.nix-daemon.enable = true;
   programs.nix-index.enable = true;
+  environment.pathsToLink = ["/share/hunspell"];
+  environment.systemPackages =
+    [ pkgs.hunspell
+      pkgs.hunspellDicts.en_US
+    ];
 }
