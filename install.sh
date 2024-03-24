2a35a7adec0c0b76652e4218ffd808c3984b0585
Makoto Morinaga <makoto@mkt3.dev>
2024-03-24 15:35:48 +0900

    Update misc

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ INSTALL_DIR="${INSTALL_DIR:-$HOME/workspace/ghq/github.com/mkt3/dotfiles}"
 if [ ! -d "$INSTALL_DIR" ]; then
     echo "Cloning dotfiles..."
     git clone https://github.com/mkt3/dotfiles "$INSTALL_DIR"
+    /usr/bin/env bash "$(INSTALL_DIR)/scripts/install_essential_packages.sh"
 fi
 
 make -C "$INSTALL_DIR"
