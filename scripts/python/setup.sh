00bff12eebc15c29e850ebffc190279f2d69a201
Makoto Morinaga <makoto@mkt3.dev>
2024-04-21 15:00:17 +0900

    debug

--- scripts/python/setup.sh
+++ scripts/python/setup.sh
@@ -4,13 +4,13 @@ set -eu
 
 post_setup_poetry() {
     info "Setting global config"
+    echo $PATH
+    which poetry | true
+    which htop | true
     poetry config virtualenvs.in-project true
     poetry config virtualenvs.prefer-active-python true
 
     info "Enable completions"
-    echo $PATH
-    which poetry | true
-    which htop | true
     poetry completions zsh > "${ZSH_COMPLETION_DIR}/_poetry"
 }
 
