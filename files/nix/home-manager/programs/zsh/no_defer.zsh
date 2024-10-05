73218dc07ec612742a63dc71d3046c5194fb812b
Makoto Morinaga <makoto@mkt3.dev>
2024-10-05 19:55:30 +0900

    Update misc

--- files/nix/home-manager/programs/zsh/no_defer.zsh
+++ files/nix/home-manager/programs/zsh/no_defer.zsh
@@ -106,8 +106,8 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 
 # Command history
-HISTFILE=$XDG_STATE_HOME/zsh/history
-mkdir -p "$(dirname "$HISTFILE")"
+# HISTFILE=$XDG_STATE_HOME/zsh/history
+# mkdir -p "$(dirname "$HISTFILE")"
 HISTSIZE=50000
 SAVEHIST=50000
 setopt hist_ignore_dups
