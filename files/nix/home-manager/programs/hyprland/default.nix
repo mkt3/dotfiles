fb100c0144656db430d900fe409cc516d7f5ad7b
Makoto Morinaga <makoto@mkt3.dev>
2024-11-23 23:17:58 +0900

    Update misc

--- files/nix/home-manager/programs/hyprland/default.nix
+++ files/nix/home-manager/programs/hyprland/default.nix
@@ -139,12 +139,13 @@
           clickfinger_behavior = true;
           scroll_factor = 1;
         };
-
-        device = {
-          name = "yowkees-keyball44-mouse";
-          sensitivity = -0.5;
-        };
       };
+
+      device = {
+        name = "yowkees-keyball44-mouse";
+        sensitivity = -0.5;
+      };
+
       general = {
         gaps_in = 2;
         gaps_out = 2;
