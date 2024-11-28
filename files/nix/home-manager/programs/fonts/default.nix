6ed8870a4daa28f0ebac23521c9544d0e8342889
Makoto Morinaga <makoto@mkt3.dev>
2024-11-28 20:34:31 +0900

    Update misc

--- files/nix/home-manager/programs/fonts/default.nix
+++ files/nix/home-manager/programs/fonts/default.nix
@@ -1,22 +1,23 @@
 { pkgs, ... }:
 {
-  fonts.fontconfig.enable = true;
-
-  fonts.fontconfig.defaultFonts = pkgs.lib.mkIf (pkgs.stdenv.hostPlatform.isLinux) {
-    serif = [
-      "Noto Serif"
-      "Noto Color Emoji"
-    ];
-    sansSerif = [
-      "Noto Sans CJK JP"
-      "Noto Sans"
-      "Noto Color Emoji"
-    ];
-    monospace = [
-      "Noto Sans Mono"
-      "Noto Color Emoji"
-    ];
-    emoji = [ "Noto Color Emoji" ];
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts = pkgs.lib.mkIf (pkgs.stdenv.hostPlatform.isLinux) {
+      serif = [
+        "Noto Serif"
+        "Noto Color Emoji"
+      ];
+      sansSerif = [
+        "Noto Sans CJK JP"
+        "Noto Sans"
+        "Noto Color Emoji"
+      ];
+      monospace = [
+        "Noto Sans Mono"
+        "Noto Color Emoji"
+      ];
+      emoji = [ "Noto Color Emoji" ];
+    };
   };
 
   home.packages = with pkgs; [
