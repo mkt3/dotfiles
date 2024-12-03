2518a99c0bce853f1d515a01807432480136da0b
Makoto Morinaga <makoto@mkt3.dev>
2024-12-03 14:36:36 +0900

    Refactor installation script and update package management

--- install.sh
+++ install.sh
@@ -2,12 +2,14 @@
 
 INSTALL_DIR="${INSTALL_DIR:-$HOME/workspace/ghq/github.com/mkt3/dotfiles}"
 
-. "${INSTALL_DIR}/files/nix/home-manager/programs/zsh/path.zsh"
 
 if [ ! -d "$INSTALL_DIR" ]; then
     echo "Cloning dotfiles..."
     git clone https://github.com/mkt3/dotfiles "$INSTALL_DIR"
-    REPO_DIR="$INSTALL_DIR" /usr/bin/env bash "${INSTALL_DIR}/scripts/install_essential_packages.sh"
 fi
+
+. "${INSTALL_DIR}/files/nix/home-manager/programs/zsh/path.zsh"
+
+[ ! -d "$INSTALL_DIR" ] && REPO_DIR="$INSTALL_DIR" /usr/bin/env bash "${INSTALL_DIR}/scripts/install_essential_packages.sh"
 
 make -C "$INSTALL_DIR"
