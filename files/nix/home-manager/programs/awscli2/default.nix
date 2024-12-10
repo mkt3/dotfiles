1f158c830b92f7dc052282e1d14105237a05e09b
Makoto Morinaga <makoto@mkt3.dev>
2024-12-10 22:48:48 +0900

    Update misc

--- files/nix/home-manager/programs/awscli2/default.nix
+++ files/nix/home-manager/programs/awscli2/default.nix
@@ -4,7 +4,7 @@
 
   home.file.".zshenv".text = ''
     # aws cli
-    export AWS_CONFIG_FILE="''$XDG_CONFIG_HOME"/aws/config
-    export AWS_SHARED_CREDENTIALS_FILE="''$XDG_CONFIG_HOME"/aws/credentials
+    export AWS_CONFIG_FILE="''${XDG_CONFIG_HOME}/aws/config"
+    export AWS_SHARED_CREDENTIALS_FILE="''${XDG_CONFIG_HOME}/aws/credentials"
   '';
 }
