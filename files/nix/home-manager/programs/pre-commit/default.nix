65c6a2b70de66ebf9a6c26232d95c6ad103748bd
Makoto Morinaga <makoto@mkt3.dev>
2025-02-18 05:40:21 +0900

    Enable pre-commit template

--- files/nix/home-manager/programs/pre-commit/default.nix
+++ files/nix/home-manager/programs/pre-commit/default.nix
@@ -1,4 +1,18 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   home.packages = [ pkgs.pre-commit ];
+
+  programs.git = {
+    extraConfig = {
+      init = {
+        templatedir = "${config.home.homeDirectory}/.config/git/git-templates/pre-commit";
+      };
+    };
+  };
+
+  xdg.configFile."git/git-templates/pre-commit" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/nix/home-manager/programs/pre-commit/git-templates/pre-commit";
+    recursive = true;
+  };
+
 }
