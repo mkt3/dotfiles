4db3cec5ff0e3b22e453ae1ba56b32db2c10db95
Makoto Morinaga <makoto@mkt3.dev>
2025-05-06 01:13:40 +0900

    Update mako

--- files/nix/home-manager/programs/mako/default.nix
+++ files/nix/home-manager/programs/mako/default.nix
@@ -2,31 +2,39 @@
 {
   services.mako = {
     enable = true;
-    sort = "-time";
-    layer = "overlay";
-    backgroundColor = "#4c566a";
-    width = 700;
-    height = 220;
-    borderSize = 2;
-    borderColor = "#88c0d0";
-    borderRadius = 5;
-    icons = true;
-    maxIconSize = 64;
-    defaultTimeout = 5000;
-    ignoreTimeout = true;
-    padding = "14";
-    font = "plemoljp-nf 14";
-    margin = "20";
-    extraConfig = ''
-      [urgency=low]
-      border-color=#81a1c1
 
-      [urgency=normal]
-      border-color=#88c0d0
+    # settings（グローバルな設定）は key = value の形で書く
+    settings = {
+      sort = "-time";
+      layer = "overlay";
+      backgroundColor = "#4c566a";
+      width = "700";
+      height = "220";
+      borderSize = "2";
+      borderColor = "#88c0d0";
+      borderRadius = "5";
+      icons = "true";
+      maxIconSize = "64";
+      defaultTimeout = "5000";
+      ignoreTimeout = "true";
+      padding = "14";
+      font = "plemoljp-nf 14";
+      margin = "20";
+    };
 
-      [urgency=high]
-      border-color=#bf616a
-      default-timeout=0
-    '';
+    criteria = {
+      "urgency=low" = {
+        border-color = "#81a1c1";
+      };
+
+      "urgency=normal" = {
+        border-color = "#88c0d0";
+      };
+
+      "urgency=high" = {
+        border-color = "#bf616a";
+        default-timeout = "0";
+      };
+    };
   };
 }
