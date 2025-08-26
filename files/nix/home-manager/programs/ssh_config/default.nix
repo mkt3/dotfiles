18d8e60efaa966d9839a5addb371ea4cae039726
Makoto Morinaga <makoto@mkt3.dev>
2025-08-27 08:48:36 +0900

    Configure SSH client
    - Disable default SSH config
    - Add common configuration for all hosts
    - Move SSH_AUTH_SOCK definition to .zshenv file

--- files/nix/home-manager/programs/ssh_config/default.nix
+++ files/nix/home-manager/programs/ssh_config/default.nix
@@ -2,23 +2,32 @@
 {
   programs.ssh = {
     enable = true;
+    enableDefaultConfig = false;
     includes = [ "~/.ssh/extra_config" ];
-    serverAliveCountMax = 5;
-    serverAliveInterval = 60;
-    hashKnownHosts = true;
+    matchBlocks."*" = {
+      forwardAgent = false;
+      serverAliveInterval = 60;
+      serverAliveCountMax = 5;
+      compression = false;
+      addKeysToAgent = "no";
+      hashKnownHosts = true;
+      userKnownHostsFile = "~/.ssh/known_hosts";
+      controlMaster = "no";
+      controlPath = "~/.ssh/master-%r@%n:%p";
+      controlPersist = "no";
+    };
   };
 
-  home.file.".zshenv".text =
-    ''
-      # ssh
-      export SSH_AGENT_PID=""
-    ''
-    + (
-      if pkgs.stdenv.hostPlatform.isLinux then
-        "export SSH_AUTH_SOCK=\"\${XDG_RUNTIME_DIR}/gnupg/S.gpg-agent.ssh\"\n"
-      else if pkgs.stdenv.hostPlatform.isDarwin then
-        "export SSH_AUTH_SOCK=\"\${HOME}/.gnupg/S.gpg-agent.ssh\"\n"
-      else
-        ""
-    );
+  home.file.".zshenv".text = ''
+    # ssh
+    export SSH_AGENT_PID=""
+  ''
+  + (
+    if pkgs.stdenv.hostPlatform.isLinux then
+      "export SSH_AUTH_SOCK=\"\${XDG_RUNTIME_DIR}/gnupg/S.gpg-agent.ssh\"\n"
+    else if pkgs.stdenv.hostPlatform.isDarwin then
+      "export SSH_AUTH_SOCK=\"\${HOME}/.gnupg/S.gpg-agent.ssh\"\n"
+    else
+      ""
+  );
 }
