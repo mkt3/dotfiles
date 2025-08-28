1214d35f52d54dac1050016013d7b4b08e12fa4a
Makoto Morinaga <makoto@mkt3.dev>
2025-08-28 21:27:07 +0900

    Update Emacs unstable with system appearance patch
    Updated emacs-unstable.nix to apply system appearance patch and
    round-undecorated-frame patch for emacs 30.

--- files/nix/home-manager/overlays/patched-emacs/emacs-unstable.nix
+++ files/nix/home-manager/overlays/patched-emacs/emacs-unstable.nix
@@ -8,18 +8,13 @@ final: prev: {
         sha256 = "+z/KfsBm1lvZTZNiMbxzXQGRTjkCFO4QPlEK35upjsE=";
       })
       # Make Emacs aware of OS-level light/dark mode
-      # (final.fetchpatch {
-      #   url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-28/system-appearance.patch";
-      #   sha256 = "3QLq91AQ6E921/W9nfDjdOUWR8YVsqBAT/W9c1woqAw=";
-      # })
-      # # Use poll instead of select to get file descriptors
-      # (final.fetchpatch {
-      #   url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-29/poll.patch";
-      #   sha256 = "HPuHrsKq17ko8xP8My+IYcJV+PKio4jK41qID6QFXFs=";
-      # })
+      (final.fetchpatch {
+        url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-30/system-appearance.patch";
+        sha256 = "3QLq91AQ6E921/W9nfDjdOUWR8YVsqBAT/W9c1woqAw=";
+      })
       # Enable rounded window with no decoration
       (final.fetchpatch {
-        url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-29/round-undecorated-frame.patch";
+        url = "https://raw.githubusercontent.com/d12frosted/homebrew-emacs-plus/master/patches/emacs-30/round-undecorated-frame.patch";
         sha256 = "uYIxNTyfbprx5mCqMNFVrBcLeo+8e21qmBE3lpcnd+4=";
       })
     ];
