371af5256bb0a1463c492001851f08efdb548553
Makoto Morinaga <makoto@mkt3.dev>
2025-12-23 00:38:29 +0900

    Update misc

--- files/nix/home-manager/programs/recoll/default.nix
+++ files/nix/home-manager/programs/recoll/default.nix
@@ -1,18 +1,45 @@
 { config, pkgs, ... }:
+
+let
+  isDarwin = pkgs.stdenv.hostPlatform.isDarwin;
+
+  dataDir =
+    if isDarwin
+    then "~/GoogleDrive/local_data_dir"
+    else "~/Nextcloud";
+
+  recollHelperPath =
+    if isDarwin
+    then "recollhelperpath = /opt/homebrew/bin"
+    else "";
+
+  recollPathEnv =
+    if isDarwin
+    then ''
+      export PATH="''${PATH}:/Applications/Recoll.app/Contents/MacOS"
+    ''
+    else "";
+in
 {
-  home.packages = pkgs.lib.mkIf (!pkgs.stdenv.hostPlatform.isDarwin) [ pkgs.recoll ];
+  home.packages = pkgs.lib.mkIf (!isDarwin) [ pkgs.recoll ];
 
   xdg.configFile."recoll/recoll.conf" = {
     text = ''
-      topdirs = ~/Nextcloud/book ~/Nextcloud/orgnotes ~/Nextcloud/documents ~/Nextcloud/zotero
-      recollhelperpath = /opt/homebrew/bin
-      noContentSuffixes+ = .bib
+      topdirs = ${dataDir}/book ${dataDir}/orgnotes ${dataDir}/documents ${dataDir}/zotero
+      ${recollHelperPath}
+      noContentSuffixes+ = .bib .zip .iso .img
+      underscoreasletter = true
+      backslashasletter = true
+      orgmodesubdocs = true
+      indexallfilenames = true
+      indexStoreDocText = true
+      idxabsmlen = 2000
     '';
   };
 
   home.file.".zshenv".text = ''
     # recoll
     export RECOLL_CONFDIR="${config.xdg.configHome}/recoll"
-    export PATH="''${PATH}:/Applications/Recoll.app/Contents/MacOS"
+    ${recollPathEnv}
   '';
 }
