7af55f793e805fb374471bd4e492e10f00e59dfc
Makoto Morinaga <makoto@mkt3.dev>
2025-12-31 20:12:45 +0900

    Update misc

--- files/nix/systems/nixos/programs/hyprland/default.nix
+++ files/nix/systems/nixos/programs/hyprland/default.nix
@@ -39,7 +39,7 @@
 
   programs.thunar = {
     enable = true;
-    plugins = with pkgs.xfce; [
+    plugins = with pkgs; [
       thunar-archive-plugin
       thunar-volman
       thunar-media-tags-plugin
