d6e6a4303b9e81535f4ae77f5aa1117771e94c36
Makoto Morinaga <makoto@mkt3.dev>
2026-03-13 16:27:47 +0900

    Fix typo

--- files/nix/home-manager/programs/zsh/path.zsh
+++ files/nix/home-manager/programs/zsh/path.zsh
@@ -45,6 +45,5 @@ else # for linux (except NIXOS)
     export LOCALE_ARCHIVE=/usr/lib/locale/locale-archive
 
     export PATH="/usr/local/cuda/bin:${PATH}"
-    export LD_LIBRARY_PATH="/usr/local/cuda/lib64:${LD_LIBRARY_PATH}"
 fi
 export PATH="${HOME}/.local/bin:${PATH}"
