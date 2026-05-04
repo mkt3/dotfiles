15dd370b1184885cf63590edf747a79e762cbbdd
Makoto Morinaga <makoto@mkt3.dev>
2026-05-05 08:18:29 +0900

    Update misc

--- nix/home-manager/programs/claude-code/default.nix
+++ nix/home-manager/programs/claude-code/default.nix
@@ -2,11 +2,5 @@
 {
   programs.claude-code = {
     enable = true;
-    settings = {
-      "$schema" = "https://json.schemastore.org/claude-code-settings.json";
-      "env" = {
-        "CLAUDE_CODE_DISABLE_NONESSENTIAL_TRAFFIC" = "1";
-      };
-    };
   };
 }
