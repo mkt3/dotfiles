d207099f3a23bbeb8d54c6a2d50091d89ddfa350
Makoto Morinaga <makoto@mkt3.dev>
2026-05-22 23:04:49 +0900

    Update omniwm config

--- nix/home-manager/programs/omniwm/default.nix
+++ nix/home-manager/programs/omniwm/default.nix
@@ -1,7 +1,13 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 let
   omniwm = pkgs.callPackage ../../packages/omniwm { };
+  dotfilesDir = "${config.home.homeDirectory}/workspace/ghq/github.com/mkt3/dotfiles";
 in
 {
   home.packages = [ omniwm ];
+
+  xdg.configFile."omniwm" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/nix/home-manager/programs/omniwm/config";
+    force = true;
+  };
 }
