c7d628f5ba1cf56f0777e086701f51ea8be839f5
Makoto Morinaga <makoto@mkt3.dev>
2026-09-02 10:08:38 +0900

    nix: share nixpkgs with noctalia

--- nix/flake.source.nix
+++ nix/flake.source.nix
@@ -58,6 +58,7 @@
     };
     noctalia = {
       url = "github:noctalia-dev/noctalia";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     noctalia-greeter = {
       url = "github:noctalia-dev/noctalia-greeter";
