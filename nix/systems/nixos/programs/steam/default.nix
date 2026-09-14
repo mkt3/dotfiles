67c2f49d0839276d56e86ffd78d28e5a15526469
Makoto Morinaga <makoto@mkt3.dev>
2026-09-15 08:50:22 +0900

    Add gamescope

--- nix/systems/nixos/programs/steam/default.nix
+++ nix/systems/nixos/programs/steam/default.nix
@@ -5,4 +5,16 @@
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
   };
+
+  # programs.gamemode.enable = true;
+
+  programs.gamescope = {
+    enable = true;
+    capSysNice = true;
+  };
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
 }
