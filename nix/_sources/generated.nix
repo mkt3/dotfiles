9d1b15391abb2680832ccff74e82665d1cc7d984
Makoto Morinaga <makoto@mkt3.dev>
2026-09-21 05:31:29 +0900

    chore: update OmniWM to 0.7.1

--- nix/_sources/generated.nix
+++ nix/_sources/generated.nix
@@ -47,10 +47,10 @@
   };
   omniwm = {
     pname = "omniwm";
-    version = "v0.7.0";
+    version = "v0.7.1";
     src = fetchurl {
-      url = "https://github.com/BarutSRB/OmniWM/releases/download/v0.7.0/OmniWM-v0.7.0.zip";
-      sha256 = "sha256-+54G9cf1Q/3kuoOP7YNQaorHKylwce29SPT54MA2yrY=";
+      url = "https://github.com/BarutSRB/OmniWM/releases/download/v0.7.1/OmniWM-v0.7.1.zip";
+      sha256 = "sha256-cTXVIMBf1wEt6Mm3WgvZbT+/rYERvie7XDACtgz3YWI=";
     };
   };
 }
